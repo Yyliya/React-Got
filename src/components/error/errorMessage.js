b854631deb87cb9383445b62b8f1c5d7e2af0eb0
Yuliya <yuliya.zainab@gmail.com>
2021-01-16 11:46:22 +0200

    random-char work

---
+++ src/components/error/errorMessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img from './error.jpg';
+import './errorMessage'
+
+const ErrorMessage = () => {
+    return (
+        <>
+            <img src={img} alt='error'></img>
+            <span>Something goes wrong :(</span>
+        </>
+    )
+    
+}
+export default ErrorMessage;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-dgpsen0p6ma"><div className="ldio-ey0s1ct85b7">
+        <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner
